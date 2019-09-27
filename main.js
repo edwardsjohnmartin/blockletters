99b06ec47f254ccde0536c79598abcccb7247187
John Edwards <edwardsjohnmartin@gmail.com>
2019-09-27 07:22:29 -0600

    new additions

---
+++ main.js
@@ -0,0 +1,22 @@
+function init() {
+  let str = '17 z q u x
+14 t w y n
+4 i q a 4
+9 a b c d
+1 d h n butterfly
+11 h e l r
+8 r h o v
+2 e i p holly
+3 p m g flag
+12 w s n d
+7 e t j clover
+10 g c y mapleleaf
+15 t o v u
+5 l o r heart
+18 b f j k
+16 i f k m
+6 s a l w
+13 a o s c';
+
+  console.log(str);
+}
